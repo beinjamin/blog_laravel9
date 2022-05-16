be2e292e0c9efb20a9d4a317d48c27c03ee91423
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-16 19:17:11 +0100

    Creation du model category

--- database/migrations/2022_05_16_175940_create_posts_table.php
+++ database/migrations/2022_05_16_175940_create_posts_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
 
 
             $table->foreignIdFor(User::class);
-            $table->foreignIdFor(cate::class);
+            $table->foreignIdFor(Category::class);
 
 
         });
