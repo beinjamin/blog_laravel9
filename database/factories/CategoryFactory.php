f8029623238fd276018ce33dcc4993911c323101
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-16 19:33:57 +0100

    Creation de la factory categorie

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,8 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "name" => $this->name();
+            
         ];
     }
 }
